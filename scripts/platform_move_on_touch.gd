575b72a8534d2c49b8b058cf0eb16f95640db13e
Mikel Hamer <mikelhamer@gmail.com>
2024-07-04 03:28:09 -0400

    e?

--- scripts/platform_move_on_touch.gd
+++ scripts/platform_move_on_touch.gd
@@ -1,19 +1,20 @@
 extends Node
 
-@onready var static_body_2d = $StaticBody2D
-var player_on_platform = false
-@onready var this = $"."
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
+@onready var timer = $Timer
+var moving = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if (player_on_platform):
+	if (moving):
 		self.position.x -= 120 * delta
 
 func _on_area_2d_body_entered(body):
-	player_on_platform = true
+	timer.start(1)
+
+
+func _on_timer_timeout():
+	moving = true
+
+
+func _on_area_2d_body_exited(body):
+	timer.stop()
