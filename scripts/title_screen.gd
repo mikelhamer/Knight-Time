a04f8d570c62d5c629f192cb2a924bd284c12b2e
Mikel Hamer <mikelhamer@gmail.com>
2024-07-10 23:31:31 -0400

    set title as main scene, fix scene reference to level 1

--- scripts/title_screen.gd
+++ scripts/title_screen.gd
@@ -4,4 +4,4 @@ extends Node2D
 func _input(event):
 	if event is InputEventKey:
 		if event.pressed:
-			get_tree().change_scene_to_file("res://scenes/level_1.tscn")
+			get_tree().change_scene_to_file("res://scenes/game.tscn")
