884b6b538cbf84a870f290403c0d7ee63df252a2
Mikel Hamer <mikelhamer@gmail.com>
2024-07-14 11:33:57 -0400

    falling platforms!

---
+++ scripts/falling_block.gd
@@ -0,0 +1,17 @@
+extends StaticBody2D
+
+const SPEED = 300
+const FALL_TRIGGER_TIMEOUT = .25
+
+var falling = false
+
+func _process(delta):
+	if falling:
+		position.y += SPEED * delta
+	
+
+func _on_fall_trigger_body_entered(body):
+	$Timer.start(FALL_TRIGGER_TIMEOUT)
+
+func _on_timer_timeout():
+	falling = true
