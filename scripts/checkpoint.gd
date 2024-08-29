829b36e44d1779229604e8cb20a83fd8fa3a26cc
Mikel Hamer <mikelhamer@gmail.com>
2024-08-29 14:34:24 -0400

    only reach checkpoint once

--- scripts/checkpoint.gd
+++ scripts/checkpoint.gd
@@ -2,5 +2,9 @@ extends Area2D
 
 signal checkpoint_reached
 
+var reached = false
+
 func _on_body_entered(body):
-	checkpoint_reached.emit(self)
+	if !reached:
+		reached = true
+		checkpoint_reached.emit(self)
