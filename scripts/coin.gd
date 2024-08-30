a40d9d2cd6b91b9bf2c33718cab8dc45e0c876c4
Mikel Hamer <mikelhamer@gmail.com>
2024-08-30 12:56:36 -0400

    coin id

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -2,6 +2,14 @@ extends Area2D
 
 @onready var animation_player = $AnimationPlayer
 
+static var id_sequence = 0
+
+var id: int
+
+func _ready():
+	id_sequence += 1
+	id = id_sequence
+
 func _on_body_entered(body):
 	animation_player.play("coin_get")
 	Game.add_level_coin()
