e9c72911db64422f0d9525f1bced378978fe155d
Mikel Hamer <mikelhamer@gmail.com>
2024-09-04 00:11:58 -0400

    coin get label, purps in 3

--- levels/credits.gd
+++ levels/credits.gd
@@ -6,7 +6,7 @@ extends CanvasLayer
 func _ready():
 	coins_label.text = "Coins: " + str(Game.total_coins.size()) + "/202"
 	if Game.total_coins.size() == 202:
-		congrats_label = 'AMAZING!!!'
+		congrats_label.text = 'AMAZING!!!'
 	
 	
 func _on_button_button_up():
