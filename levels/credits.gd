19e29b1f609814ec6559886765ea1385237dd29c
Mikel Hamer <mikelhamer@gmail.com>
2024-09-03 23:42:10 -0400

    level improvements and coin counter in credits

--- levels/credits.gd
+++ levels/credits.gd
@@ -1,5 +1,14 @@
 extends CanvasLayer
 
+@onready var coins_label = $CoinsLabel
+@onready var congrats_label = $CongratsLabel
+
+func _ready():
+	coins_label.text = "Coins: " + str(Game.total_coins.size()) + "/202"
+	if Game.total_coins.size() == 202:
+		congrats_label = 'AMAZING!!!'
+	
+	
 func _on_button_button_up():
 	Game.reset()
 	get_tree().change_scene_to_file("res://scenes/main.tscn")
